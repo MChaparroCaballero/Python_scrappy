73ba10a35309911c0eb4fcee1d0838f245b3fc8e
María <mchaparrocaballero@gmail.com>
2025-11-01 20:43:14 +0100

    first commit

---
+++ get_data.py
@@ -0,0 +1,34 @@
+#autor : Maria Chaparro 
+# git : MChaparroCaballero
+import os
+import sys
+import requests  # Librería para hacer peticiones HTTP
+from bs4 import BeautifulSoup  # Librería para analizar HTML
+from bs4.element import Tag  # Tipo para elementos HTML
+from typing import cast  # Función para convertir tipos
+
+url= "http://127.0.0.1:5500/ejer__4_scrappy/periodicos.html" # URL del sitio web local
+#LIMPIAR PANTALLA en windows necesitas cls para borrar la pantalla,
+#  y clear para linux, por eso debemos especificar que sistema operativo es con os 
+# y dependiendo de lo que nos devuelva poner uno o otro
+os.system('cls' if os.name == 'nt' else 'clear')
+
+try:
+
+    #primero hacemos la peticion y obtenemos el contenido HTML de toda la pagina
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    #luego buscamos el elemento por su id y lo casteamos para que no de error
+    el= cast(Tag,soup.find(id="exclusiva-diaria"))
+    #finalmente imprimimos el texto del elemento encontrado
+    print(f"Noticia de ultima hora: {el.get_text(strip=True)}") #ponemos el strip true para que quite los espacios
+    #en blanco al inicio y al final y los dobles saltos de linea
+
+
+except requests.RequestException as e:
+    print("Error en la petición , no se ha encontrado la url")
+except AttributeError as e:
+    print("Error al encontrar el elemento")
+except ModuleNotFoundError as e:
+    print("Módulo no encontrado: live server no activado", e)
+except Exception as e:
+    print("Error inesperado:", e)
